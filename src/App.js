f1eb41a4670e86cd24640e312176cb3b49d0f984
Mike Plis <michael.plis01@gmail.com>
2019-09-03 20:51:00 -0400

    Implement border radius for 4 corners

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+function Box({ topLeft, topRight, bottomRight, bottomLeft, style }) {
+    return (
+        <div
+            style={{
+                backgroundColor: 'lightblue',
+                border: '10px solid dodgerblue',
+                width: '300px',
+                height: '300px',
+                borderTopLeftRadius: topLeft,
+                borderTopRightRadius: topRight,
+                borderBottomRightRadius: bottomRight,
+                borderBottomLeftRadius: bottomLeft,
+                ...style
+            }}
+        />
+    );
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [topLeft, setTopLeft] = React.useState('0');
+    const [topRight, setTopRight] = React.useState('0');
+    const [bottomRight, setBottomRight] = React.useState('0');
+    const [bottomLeft, setBottomLeft] = React.useState('0');
+
+    return (
+        <div style={{ margin: '1rem' }}>
+            <h1>Border Radius Previewer</h1>
+            <div
+                style={{
+                    display: 'grid',
+                    /* prettier-ignore */
+                    gridTemplateAreas: `
+                        'topLeft . topRight'
+                        '. box .'
+                        'bottomLeft . bottomRight'
+                    `
+                }}
+            >
+                <label style={{ gridArea: 'topLeft' }}>
+                    Top Left <input value={topLeft} onChange={e => setTopLeft(e.target.value)} />
+                </label>
+
+                <label style={{ gridArea: 'topRight' }}>
+                    Top Right <input value={topRight} onChange={e => setTopRight(e.target.value)} />
+                </label>
+
+                <Box
+                    style={{ gridArea: 'box' }}
+                    topLeft={topLeft}
+                    topRight={topRight}
+                    bottomRight={bottomRight}
+                    bottomLeft={bottomLeft}
+                />
+                <label style={{ gridArea: 'bottomLeft' }}>
+                    Bottom Left{' '}
+                    <input value={bottomLeft} onChange={e => setBottomLeft(e.target.value)} />
+                </label>
+
+                <label style={{ gridArea: 'bottomRight' }}>
+                    Bottom Right{' '}
+                    <input value={bottomRight} onChange={e => setBottomRight(e.target.value)} />
+                </label>
+            </div>
+        </div>
+    );
 }
 
 export default App;
